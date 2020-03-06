77438468584d6f36618bdc1321d212dec1e1b3b4
Sully789 <43644133+Sully789@users.noreply.github.com>
2020-03-06 19:31:05 +0000

    Sync

---
+++ FYP/Assets/Scripts/PlayerAI_MCTS.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAI_MCTS : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //
+    void MCTS()
+    {
+        //While game active... in Update method?
+            //leaf = navigation(root)
+            //simulation result = rollout(leaf)
+            //backprogagate (leaf, simulation result)
+
+        //return best child(root)
+    }
+
+    //Method for node traversal 
+    void Navigate()
+    {
+        //While fully expanded(node)
+            //node = best UCT(node)
+        
+        //if no children present, node is terminal
+            //return unvisited node OR node
+    }
+
+    //Method for the result of the simulation 
+    void Rollout()
+    {
+        //While not a terminal node
+            //Node = rollout policy(node)
+
+        //return result(node)
+    }
+
+    //Method for randomly selecting a child node 
+    void RolloutPoilicy()
+    {
+        //return Random Node(children.node) 
+    }
+
+    //Method for backpropagation 
+    void Backpropagation()
+    {
+        //if root node
+            //return
+        //node.stats = update_stats(node, result)
+        //backpropagate(node.parent) 
+    }
+
+    //Method for selecting the best child 
+    void BestChild()
+    {
+        //pick child with highest number of visits
+    }
+}
